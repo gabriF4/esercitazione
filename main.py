e8607ba106b383c1b159274ea1054f913a50f20c
Gabriele <gabbopg13@gmail.com>
2020-03-25 13:16:04 +0100

    modificato main.py

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ n = input("nome ")
 c = input("cognome ")
 a = int(input("anni "))
 print("ciao {} {} di {} anni".format(n, c, a)
+m = input("la tua materia preferita__")
+if m == "informatica":
+    print("molto bravo")
+else:
+    print("molto male")
 
